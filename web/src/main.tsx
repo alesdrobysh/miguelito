88983292dedce02401b66e9e377a67cb906b02d2
Ales Drobysh <drobysh.ales@gmail.com>
2026-06-03 22:43:36 +0200

    chore(web): remove Vercel analytics and deployment config

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -5,13 +5,11 @@ if (typeof globalThis.Buffer === 'undefined') {
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <Analytics />
   </StrictMode>,
 )
